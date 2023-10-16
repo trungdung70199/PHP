df69aec9e389edd580bbd41dcab42d9a992b9a5e
trungdung70199 <trungdung70199@gmail.com>
2023-10-16 09:53:19 +0900

    up file

---
+++ laravel/routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+// Make about page
+Route::get('/about', function () {
+    return "This is About Page.";
+});
+
+// Make items/xx page
+Route::get('/items', function() {
+    $message = "Product ID is {$id}";
+    return $message;
+});
+
+// Amazon routing
+Route::get('/dp{id}', function ($id) {
+    $message = "Product ID is {$id}";
+    return $message;
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
